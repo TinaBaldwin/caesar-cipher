09699adc417ede72d346f0ab307fa2e60f2a4256
Tina Baldwin <OTBaldwin@gmail.com>
2024-11-03 22:50:35 -0500

    Caesar-cipher code completed

--- cipher.py
+++ cipher.py
@@ -1,1 +1,20 @@
 # add your code here
+
+result = ""
+shift = 5
+
+sentence = str(input("Please enter a sentence: "))
+
+for character in sentence:
+    if character.isalpha(): 
+        shift_alpha = ord(character) + shift 
+        if character.islower() and shift_alpha > ord("z"):
+           shift_alpha -= 26        
+        elif character.isupper() and shift_alpha > ord("Z"):
+            shift_alpha -= 26
+        encrypted_letter = chr(shift_alpha)
+        result += encrypted_letter
+    else:
+        result += character 
+
+print("The encrypted sentence is: ", result)
